697c5605b68b34697c93a87217bdc37b00572436
Akash Bujagale <akash.bujagale@gmail.com>
2025-01-12 23:28:24 +0530

    add readme file

---
+++ src/app/core/firebase/firebase-config.const.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBWoY7dn5UyzFTGcIkCZ1av3EDQmvSHHCs",
+  authDomain: "akashbujagale-2c354.firebaseapp.com",
+  projectId: "akashbujagale-2c354",
+  storageBucket: "akashbujagale-2c354.firebasestorage.app",
+  messagingSenderId: "271258284321",
+  appId: "1:271258284321:web:535f67e1b17225fda54e60",
+  measurementId: "G-HZ0E58JR6H"
+};
---
+++ src/app/features/projects/projects.component.ts
@@ -0,0 +1,52 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss',
+})
+export class ProjectsComponent {
+  public projects = [
+    {
+      title: 'HireQuest',
+      description:
+        'A web application for job postings and job searching, featuring a user-friendly interface, full CRUD operations, and job search functionality. Tested with 500 integrated job listings for performance optimization.',
+      techList: ['React', 'Tailwind CSS', 'JSON Server'],
+      github: 'https://github.com/AkashBujagale/HireQuest',
+      screenshot: 'https://raw.githubusercontent.com/AkashBujagale/HireQuest/main/public/screen.png',
+      live: '',
+    },
+    {
+      title: 'PrimeEstate',
+      description:
+        'Designed a real estate web application with property listings, project details, photo galleries, and user profiles. Integrated maps for accurate locations, real-time chat, user authentication, and robust search functionality.',
+      techList: ['React', 'MongoDB', 'Node.js', 'Express.js', 'SCSS', 'JWT', 'Prisma'],
+      github: 'https://github.com/AkashBujagale',
+      screenshot: 'assets/prime-estate.png',
+      live: '',
+    },
+    {
+      title: 'SD BELT',
+      description:
+        'Developed an IoT solution to monitor infected patients and ensure social distancing during a hackathon. Integrated thermal and UV sensors to detect proximity and trigger alerts. Approved patent holder for this project (Application No: 337359-001)',
+      techList: ['IoT', 'CAD Design', 'Thermal Sensors', 'UV Sensors'],
+      github: '',
+      screenshot: 'assets/images.png',
+      live: '',
+    },
+  ];
+
+  public visibleProjects = this.projects.slice(0, 3);
+
+  showMoreProjects() {
+    if (this.visibleProjects.length < this.projects.length) {
+      this.visibleProjects = this.projects.slice(
+        0,
+        this.visibleProjects.length + 3
+      );
+    }
+  }
+}
